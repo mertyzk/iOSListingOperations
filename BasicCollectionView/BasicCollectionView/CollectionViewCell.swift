879c84bb04460eee6257fd4dddb9ac951e6fadad
RinKusChi <meyuzak@gmail.com>
2022-02-06 12:59:22 +0300

    6 February 2022

---
+++ BasicCollectionView/BasicCollectionView/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  BasicCollectionView
+//
+//  Created by Macbook Air on 6.02.2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var hucreLabel: UILabel!
+}
